47e00afac6e9dc89f0d40c12e03d2a99d5b943f1
Dominic Cabitac <dominic.cabitac@uoit.net>
2019-11-07 14:30:27 -0500

    Added menu pictues and swiper main menu

--- advancewars/lib/main.dart
+++ advancewars/lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'src/config.dart';
+import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 
 void main() => runApp(new MyApp());
 
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new ExampleHorizontal()
     );
   }
 }
@@ -48,5 +50,26 @@ class _MyHomePageState extends State<MyHomePage> {
 )
 ,
     );
+  }
+}
+
+class ExampleHorizontal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.asset(
+              images[index],
+              fit: BoxFit.fill,
+            );
+          },
+
+          indicatorLayout: PageIndicatorLayout.COLOR,
+          // autoplay: false,
+          itemCount: images.length,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
+        ));
   }
 }
