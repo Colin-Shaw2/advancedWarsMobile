60b7e71e473fa8bbcd512999f0d49a7893bfb158
Dominic Cabitac <dominic.cabitac@uoit.net>
2019-12-01 20:41:27 -0500

    Internationalization implemented on mapPage

--- advancewars/lib/MapPage.dart
+++ advancewars/lib/MapPage.dart
@@ -3,6 +3,9 @@ import 'package:advancewars/classes/WarMap.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'databases/saving.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({Key key, this.title}) : super(key: key);
@@ -64,23 +67,23 @@ Future<void> _menu(BuildContext context) async {
     barrierDismissible: true,
     builder: (BuildContext context) {
       return SimpleDialog(
-        title: Text('Menu'),
+        title: Text(FlutterI18n.translate(context, 'menu')),
         children: <Widget>[
           SimpleDialogOption(
-            child: Text('Give Up'),
+            child: Text(FlutterI18n.translate(context, 'giveUp')),
             onPressed: () {
               Navigator.pop(context, MenuChoice.giveUp);
             },
           ),
           SimpleDialogOption(
-            child: Text('Save'),
+            child: Text(FlutterI18n.translate(context, 'save')),
             onPressed: () {
               Saving().saveMap(currentMap);
               Navigator.pop(context, MenuChoice.save);
             },
           ),
           SimpleDialogOption(
-            child: Text('End'),
+            child: Text(FlutterI18n.translate(context, 'end')),
             onPressed: () {
               Navigator.pop(context, MenuChoice.end);
             },
