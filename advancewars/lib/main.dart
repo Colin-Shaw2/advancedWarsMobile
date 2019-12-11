89c12eb00831b9b41d95e20adf4028280469072a
Dominic Cabitac <dominic.cabitac@uoit.net>
2019-12-11 13:04:43 -0500

    Potential background music

--- advancewars/lib/main.dart
+++ advancewars/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:advancewars/options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,6 +10,9 @@ import 'MapPage.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_i18n/flutter_i18n_delegate.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/widgets.dart';
 
 
 void main() => runApp(MyApp());
@@ -48,6 +53,7 @@ class _HomeScreen extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Bgm().play("music.mp3");
     //Code for music not working currently
     // Flame.bgm.initialize();
     // Bgm audio = Bgm();
@@ -179,6 +185,92 @@ class ThirdRoute extends StatelessWidget {
     );
   }
 }
+
+class Bgm extends WidgetsBindingObserver {
+  bool _isRegistered = false;
+  AudioPlayer audioPlayer;
+  bool isPlaying = false;
+
+  /// Registers a [WidgetsBinding] observer.
+  ///
+  /// This must be called for auto-pause and resume to work properly.
+  void initialize() {
+    if (_isRegistered) {
+      return;
+    }
+    _isRegistered = true;
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  /// Dispose the [WidgetsBinding] observer.
+  void dispose() {
+    if (!_isRegistered) {
+      return;
+    }
+    WidgetsBinding.instance.removeObserver(this);
+    _isRegistered = false;
+  }
+
+  /// Plays and loops a background music file specified by [filename].
+  ///
+  /// The volume can be specified in the optional named parameter [volume]
+  /// where `0` means off and `1` means max.
+  ///
+  /// It is safe to call this function even when a current BGM track is
+  /// playing.
+  void play(String filename, {double volume}) async {
+    volume ??= 1;
+
+    if (audioPlayer != null && audioPlayer.state != AudioPlayerState.STOPPED) {
+      audioPlayer.stop();
+    }
+
+    isPlaying = true;
+    audioPlayer = await Flame.audio.loopLongAudio(
+      filename,
+      volume: volume,
+    );
+  }
+
+  /// Prefetch an audio and store it in the cache.
+  ///
+  /// Alias of `FlameAudio.load();`.
+  Future<File> load(String file) => Flame.audio.load(file);
+
+  /// Prefetch a list of audios and store them in the cache.
+  ///
+  /// Alias of `FlameAudio.loadAll();`.
+  Future<List<File>> loadAll(List<String> files) => Flame.audio.loadAll(files);
+
+  /// Clears the file in the cache.
+  ///
+  /// Alias of `FlameAudio.clear();`.
+  void clear(String file) => Flame.audio.clear(file);
+
+  /// Clears all the audios in the cache.
+  /// Alias of `FlameAudio.clearAll();`.
+  ///
+  void clearAll() => Flame.audio.clearAll();
+
+  /// Handler for AppLifecycleState changes.
+  ///
+  /// This function handles the automatic pause and resume when the app
+  /// lifecycle state changes. There is NO NEED to call this function directly
+  /// directly.
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      if (isPlaying &&
+          audioPlayer != null &&
+          audioPlayer.state == AudioPlayerState.PAUSED) {
+        audioPlayer.resume();
+      }
+    } else {
+      audioPlayer.pause();
+    }
+  }
+}
+
 
 // Future<void> _options(BuildContext context) async {
 //   showDialog<MenuChoice>(
