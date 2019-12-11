7f0846798f9b30af2bfc502fb9c2c0b1499b252e
Colin-Shaw2 <colin.shaw2@uoit.net>
2019-12-11 13:25:39 -0500

    chnaged woods map

--- advancewars/lib/classes/woodsMap.dart
+++ advancewars/lib/classes/woodsMap.dart
@@ -1,3 +1,4 @@
+import 'package:advancewars/classes/terrain/Mountain.dart';
 import 'package:advancewars/classes/terrain/Wood.dart';
 import 'package:advancewars/classes/units/Infantry.dart';
 import 'package:advancewars/classes/units/Tank.dart';
@@ -5,87 +6,89 @@ import 'package:advancewars/classes/units/Tank.dart';
 import 'terrain/Plain.dart';
 import 'Tile.dart';
 import 'WarMap.dart';
+import 'terrain/Road.dart';
+import 'units/MTank.dart';
 
 class WoodsMap extends WarMap {
   WoodsMap() : super(16, 9) {
     
-    tileMap[0][0] = new Tile(Wood());
+    tileMap[0][0] = new Tile.unit(Plain(), MTank.orange());
     tileMap[1][0] = new Tile(Wood());
     tileMap[2][0] = new Tile(Wood());
-    tileMap[3][0] = new Tile(Wood());
+    tileMap[3][0] = new Tile(Plain());
     tileMap[4][0] = new Tile(Wood());
-    tileMap[5][0] = new Tile(Wood());
+    tileMap[5][0] = new Tile(Mountain());
     tileMap[6][0] = new Tile(Wood());
-    tileMap[7][0] = new Tile(Wood());
-    tileMap[8][0] = new Tile(Wood());
+    tileMap[7][0] = new Tile(Plain());
+    tileMap[8][0] = new Tile(Road());
     tileMap[9][0] = new Tile(Wood());
     tileMap[10][0] = new Tile(Wood());
-    tileMap[11][0] = new Tile(Wood());
+    tileMap[11][0] = new Tile(Plain());
     tileMap[12][0] = new Tile(Wood());
-    tileMap[13][0] = new Tile(Wood());
+    tileMap[13][0] = new Tile(Plain());
     tileMap[14][0] = new Tile(Wood());
-    tileMap[15][0] = new Tile(Wood());
+    tileMap[15][0] = new Tile.unit(Wood(), MTank.blue());
 
     
     
-    tileMap[0][1] = new Tile(Wood());
+    tileMap[0][1] = new Tile(Plain());
     tileMap[1][1] = new Tile(Wood());
     tileMap[2][1] = new Tile(Wood());
     tileMap[3][1] = new Tile(Wood());
-    tileMap[4][1] = new Tile(Wood());
+    tileMap[4][1] = new Tile(Mountain());
     tileMap[5][1] = new Tile(Wood());
-    tileMap[6][1] = new Tile(Wood());
-    tileMap[7][1] = new Tile(Wood());
-    tileMap[8][1] = new Tile(Wood());
-    tileMap[9][1] = new Tile(Wood());
-    tileMap[10][1] = new Tile(Wood());
+    tileMap[6][1] = new Tile(Plain());
+    tileMap[7][1] = new Tile(Mountain());
+    tileMap[8][1] = new Tile(Road());
+    tileMap[9][1] = new Tile(Mountain());
+    tileMap[10][1] = new Tile(Plain());
     tileMap[11][1] = new Tile(Wood());
     tileMap[12][1] = new Tile(Wood());
-    tileMap[13][1] = new Tile(Wood());
+    tileMap[13][1] = new Tile(Plain());
     tileMap[14][1] = new Tile(Wood());
-    tileMap[15][1] = new Tile(Wood());
+    tileMap[15][1] = new Tile(Plain());
 
     
     
     tileMap[0][2] = new Tile(Wood());
-    tileMap[1][2] = new Tile(Wood());
+    tileMap[1][2] = new Tile(Plain());
     tileMap[2][2] = new Tile(Wood());
-    tileMap[3][2] = new Tile(Wood());
+    tileMap[3][2] = new Tile(Plain());
     tileMap[4][2] = new Tile(Wood());
     tileMap[5][2] = new Tile(Wood());
-    tileMap[6][2] = new Tile(Wood());
+    tileMap[6][2] = new Tile(Plain());
     tileMap[7][2] = new Tile(Wood());
-    tileMap[8][2] = new Tile(Wood());
-    tileMap[9][2] = new Tile(Wood());
+    tileMap[8][2] = new Tile(Road());
+    tileMap[9][2] = new Tile(Plain());
     tileMap[10][2] = new Tile(Wood());
     tileMap[11][2] = new Tile(Wood());
-    tileMap[12][2] = new Tile(Wood());
+    tileMap[12][2] = new Tile(Plain());
     tileMap[13][2] = new Tile(Wood());
-    tileMap[14][2] = new Tile(Wood());
-    tileMap[15][2] = new Tile(Wood());
+    tileMap[14][2] = new Tile(Plain());
+    tileMap[15][2] = new Tile(Road());
 
     
     
-    tileMap[0][3] = new Tile(Wood());
+    tileMap[0][3] = new Tile(Road());
     tileMap[1][3] = new Tile(Wood());
     tileMap[2][3] = new Tile(Wood());
-    tileMap[3][3] = new Tile(Wood());
+    tileMap[3][3] = new Tile(Plain());
     tileMap[4][3] = new Tile(Wood());
     tileMap[5][3] = new Tile(Wood());
-    tileMap[6][3] = new Tile(Wood());
-    tileMap[7][3] = new Tile(Wood());
-    tileMap[8][3] = new Tile(Wood());
+    tileMap[6][3] = new Tile(Plain());
+    tileMap[7][3] = new Tile(Mountain());
+    tileMap[8][3] = new Tile(Mountain());
     tileMap[9][3] = new Tile(Wood());
-    tileMap[10][3] = new Tile(Wood());
+    tileMap[10][3] = new Tile(Plain());
     tileMap[11][3] = new Tile(Wood());
-    tileMap[12][3] = new Tile(Wood());
+    tileMap[12][3] = new Tile(Plain());
     tileMap[13][3] = new Tile(Wood());
     tileMap[14][3] = new Tile(Wood());
     tileMap[15][3] = new Tile(Wood());
 
     
     
-    tileMap[0][4] = new Tile(Wood());
+    tileMap[0][4] = new Tile(Road());
     tileMap[1][4] = new Tile(Wood());
     tileMap[2][4] = new Tile(Wood());
     tileMap[3][4] = new Tile(Wood());
@@ -93,90 +96,90 @@ class WoodsMap extends WarMap {
     tileMap[5][4] = new Tile(Wood());
     tileMap[6][4] = new Tile(Wood());
     tileMap[7][4] = new Tile(Wood());
-    tileMap[8][4] = new Tile(Wood());
+    tileMap[8][4] = new Tile(Plain());
     tileMap[9][4] = new Tile(Wood());
     tileMap[10][4] = new Tile(Wood());
     tileMap[11][4] = new Tile(Wood());
-    tileMap[12][4] = new Tile(Wood());
+    tileMap[12][4] = new Tile(Plain());
     tileMap[13][4] = new Tile(Wood());
-    tileMap[14][4] = new Tile(Wood());
-    tileMap[15][4] = new Tile(Wood());
+    tileMap[14][4] = new Tile(Plain());
+    tileMap[15][4] = new Tile(Road());
 
     
     
-    tileMap[0][5] = new Tile(Wood());
+    tileMap[0][5] = new Tile(Road());
     tileMap[1][5] = new Tile(Wood());
     tileMap[2][5] = new Tile(Wood());
-    tileMap[3][5] = new Tile(Wood());
+    tileMap[3][5] = new Tile(Plain());
     tileMap[4][5] = new Tile(Wood());
-    tileMap[5][5] = new Tile(Wood());
+    tileMap[5][5] = new Tile(Plain());
     tileMap[6][5] = new Tile(Wood());
     tileMap[7][5] = new Tile(Wood());
-    tileMap[8][5] = new Tile(Wood());
-    tileMap[9][5] = new Tile(Wood());
+    tileMap[8][5] = new Tile(Mountain());
+    tileMap[9][5] = new Tile(Mountain());
     tileMap[10][5] = new Tile(Wood());
-    tileMap[11][5] = new Tile(Wood());
+    tileMap[11][5] = new Tile(Plain());
     tileMap[12][5] = new Tile(Wood());
     tileMap[13][5] = new Tile(Wood());
     tileMap[14][5] = new Tile(Wood());
-    tileMap[15][5] = new Tile(Wood());
+    tileMap[15][5] = new Tile(Road());
 
     
     
-    tileMap[0][6] = new Tile(Wood());
+    tileMap[0][6] = new Tile(Plain());
     tileMap[1][6] = new Tile(Wood());
-    tileMap[2][6] = new Tile(Wood());
+    tileMap[2][6] = new Tile(Plain());
     tileMap[3][6] = new Tile(Wood());
     tileMap[4][6] = new Tile(Wood());
     tileMap[5][6] = new Tile(Wood());
-    tileMap[6][6] = new Tile(Wood());
-    tileMap[7][6] = new Tile(Wood());
-    tileMap[8][6] = new Tile(Wood());
-    tileMap[9][6] = new Tile(Wood());
+    tileMap[6][6] = new Tile(Plain());
+    tileMap[7][6] = new Tile(Road());
+    tileMap[8][6] = new Tile(Road());
+    tileMap[9][6] = new Tile(Road());
     tileMap[10][6] = new Tile(Wood());
     tileMap[11][6] = new Tile(Wood());
-    tileMap[12][6] = new Tile(Wood());
+    tileMap[12][6] = new Tile(Plain());
     tileMap[13][6] = new Tile(Wood());
-    tileMap[14][6] = new Tile(Wood());
+    tileMap[14][6] = new Tile(Plain());
     tileMap[15][6] = new Tile(Wood());
 
     
     
-    tileMap[0][7] = new Tile(Wood());
+    tileMap[0][7] = new Tile(Plain());
     tileMap[1][7] = new Tile(Wood());
     tileMap[2][7] = new Tile(Wood());
     tileMap[3][7] = new Tile(Wood());
     tileMap[4][7] = new Tile(Wood());
-    tileMap[5][7] = new Tile(Wood());
-    tileMap[6][7] = new Tile(Wood());
-    tileMap[7][7] = new Tile(Wood());
-    tileMap[8][7] = new Tile(Wood());
-    tileMap[9][7] = new Tile(Wood());
-    tileMap[10][7] = new Tile(Wood());
+    tileMap[5][7] = new Tile(Road());
+    tileMap[6][7] = new Tile(Road());
+    tileMap[7][7] = new Tile(Road());
+    tileMap[8][7] = new Tile(Mountain());
+    tileMap[9][7] = new Tile(Road());
+    tileMap[10][7] = new Tile(Road());
     tileMap[11][7] = new Tile(Wood());
     tileMap[12][7] = new Tile(Wood());
     tileMap[13][7] = new Tile(Wood());
     tileMap[14][7] = new Tile(Wood());
-    tileMap[15][7] = new Tile(Wood());
+    tileMap[15][7] = new Tile(Plain());
 
     
     
-    tileMap[0][8] = new Tile(Wood());
-    tileMap[1][8] = new Tile(Wood());
-    tileMap[2][8] = new Tile(Wood());
-    tileMap[3][8] = new Tile(Wood());
-    tileMap[4][8] = new Tile(Wood());
+    tileMap[0][8] = new Tile.unit(Wood(), MTank.orange());
+    tileMap[1][8] = new Tile(Road());
+    tileMap[2][8] = new Tile(Road());
+    tileMap[3][8] = new Tile(Plain());
+    tileMap[4][8] = new Tile(Plain());
     tileMap[5][8] = new Tile(Wood());
     tileMap[6][8] = new Tile(Wood());
-    tileMap[7][8] = new Tile(Wood());
+    tileMap[7][8] = new Tile(Plain());
     tileMap[8][8] = new Tile(Wood());
-    tileMap[9][8] = new Tile(Wood());
+    tileMap[9][8] = new Tile(Mountain());
     tileMap[10][8] = new Tile(Wood());
-    tileMap[11][8] = new Tile(Wood());
-    tileMap[12][8] = new Tile(Wood());
-    tileMap[13][8] = new Tile(Wood());
-    tileMap[14][8] = new Tile(Wood());
-    tileMap[15][8] = new Tile(Wood());
+    tileMap[11][8] = new Tile(Plain());
+    tileMap[12][8] = new Tile(Plain());
+    tileMap[13][8] = new Tile(Road());
+    tileMap[14][8] = new Tile(Road());
+    tileMap[15][8] = new Tile.unit(Plain(), MTank.blue());
 
     
 
